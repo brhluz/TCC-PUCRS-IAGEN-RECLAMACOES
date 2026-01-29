49a2e89b7f77d53100ae72d9d6de7660fd5816db
Mutsubi <brh_lopes@hotmail.com>
2026-01-29 20:48:34 -0300

    ..

--- sistema-gestao-reclamacao-web/src/app/modules/client/client-routing.module.ts
+++ sistema-gestao-reclamacao-web/src/app/modules/client/client-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ComplaintFormComponent } from './components/complaint-form/complaint-form.component';
 import { ComplaintSuccessComponent } from './components/complaint-success/complaint-success.component';
-import { ComplaintStatusComponent } from './components/complaint-status/complaint-status.component';
 import { ComplaintStatusComponentV2 } from './components/complaint-status/complaint-status.componentv2';
 
 const routes: Routes = [
   { path: '', redirectTo: 'complaint-form', pathMatch: 'full' },
   { path: 'complaint-form', component: ComplaintFormComponent },
   { path: 'success/:protocol', component: ComplaintSuccessComponent },
-  { path: 'status/:protocol', component: ComplaintStatusComponent },
   { path: 'statusv2/:protocol', component: ComplaintStatusComponentV2 }
 ];
 
--- sistema-gestao-reclamacao-web/src/app/modules/client/client.module.ts
+++ sistema-gestao-reclamacao-web/src/app/modules/client/client.module.ts
@@ -4,14 +4,12 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { ClientRoutingModule } from './client-routing.module';
 import { ComplaintFormComponent } from './components/complaint-form/complaint-form.component';
 import { ComplaintSuccessComponent } from './components/complaint-success/complaint-success.component';
-import { ComplaintStatusComponent } from './components/complaint-status/complaint-status.component';
 import { ComplaintStatusComponentV2 } from './components/complaint-status/complaint-status.componentv2';
 
 @NgModule({
   declarations: [
     ComplaintFormComponent,
     ComplaintSuccessComponent,
-    ComplaintStatusComponent,
     ComplaintStatusComponentV2
   ],
   imports: [
